c7066287a995fa1ab64a3217fb04a6e031acf1c6
Adaraneo <fiftypisoftware@gmail.com>
2026-03-07 11:10:45 +0100

    SimpleIdentityGenerator test preparation

---
+++ EngineTests/IdentityGeneratorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineTests
+{
+    //[TestClass]
+    public class SimpleIdentityGeneratorTests
+    {
+        // TODO: Tests for Simple generator.
+    }
+}
